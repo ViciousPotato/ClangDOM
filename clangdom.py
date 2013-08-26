cfafc65c3ed95220e6202407199d5255223f828f
Zhou Honglin <zhou.honglin@gmail.com>
2013-08-26 23:27:19 +0800

    Add basic AST class

---
+++ clangdom.py
@@ -0,0 +1,28 @@
+# See http://clang.llvm.org/docs/IntroductionToTheClangAST.html
+
+class Decl(object):
+  """docstring for Decl"""
+  def __init__(self, arg):
+    super(Decl, self).__init__()
+    self.arg = arg
+    
+class Type(object):
+  """docstring for Type"""
+  def __init__(self, arg):
+    super(Type, self).__init__()
+    self.arg = arg
+
+class DeclContext(object):
+  """docstring for DeclContext"""
+  def __init__(self, arg):
+    super(DeclContext, self).__init__()
+    self.arg = arg
+    
+class Stmt(object):
+  """docstring for Stmt"""
+  def __init__(self, arg):
+    super(Stmt, self).__init__()
+    self.arg = arg
+
+
+    
